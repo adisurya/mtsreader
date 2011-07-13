0be51aa72c1871639a1fa3d2ae022dccdfd04150
adi surya <adi@mondial.co.id>
2011-07-13 14:14:33 +0700

    channels activity

--- src/id/co/mondial/android/mediaindonesia/MediaIndonesiaChannels.java
+++ src/id/co/mondial/android/mediaindonesia/MediaIndonesiaChannels.java
@@ -1,13 +1,30 @@
 package id.co.mondial.android.mediaindonesia;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MediaIndonesiaChannels extends Activity {
+public class MediaIndonesiaChannels extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        String[] countries = getResources().getStringArray(R.array.channels_title);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.channels, countries));        
+
+    }
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+    	
+    	super.onListItemClick(l, v, position, id);
+    	
+    	// get the item that was click
+    	String url = getResources().getStringArray(R.array.channels_url)[position];
+    	Toast.makeText(this, url, Toast.LENGTH_SHORT).show();
     }
 }
