2d7cd0c74bce98da91764b3b7c72b0daa155eb77
adi surya <adi@mondial.co.id>
2011-07-13 13:18:59 +0700

    first commit

---
+++ src/id/co/mondial/android/mediaindonesia/MediaIndonesiaChannels.java
@@ -0,0 +1,13 @@
+package id.co.mondial.android.mediaindonesia;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MediaIndonesiaChannels extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
